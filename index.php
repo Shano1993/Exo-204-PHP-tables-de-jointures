a9ced9fd01e234b278dcda0b612f02ae633c6ca1
Shano1993 <stefan.hanotiau@gmail.com>
2022-02-28 09:54:52 +0100

    php

--- index.php
+++ index.php
@@ -7,3 +7,31 @@
  * 4 - A l'aide d'un simple print_r, afficher les rôles de chaque utilisateur.
  * 5 - FIN !
  */
+
+try {
+    $server = 'localhost';
+    $db = 'exo_204';
+    $user = 'root';
+    $password = '';
+
+    $pdo = new PDO("mysql:host=$server;dbname=$db", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $request = $pdo->prepare("
+        SELECT user_role.id, role.role, user.username
+        FROM user_role
+        INNER JOIN role ON user_role.role_fk = role.id
+        INNER JOIN user ON user_role.user_fk = user.id
+    ");
+
+    if ($request->execute()) {
+        echo "<pre>";
+        print_r($request->fetchAll());
+        echo "</pre>";
+    }
+}
+
+catch (Exception $exception) {
+    echo $exception->getMessage();
+}
